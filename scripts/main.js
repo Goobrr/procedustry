f04c820d7d791b71f081e5d9a00ecb2d86d18d22
Goobrr <73060700+Goobrr@users.noreply.github.com>
2021-01-08 18:05:53 +0700

    stuff

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+const genBlocks = new Seq;
+
+//change to how many routers you want to be made
+var genAmount = 15;
+
+
+for(var i = 0; i < genAmount; i++){
+    var genID = "genRouter-" + i
+    var blockName = "Generated Router " + i
+    var b = extend(Router, genID, {
+        displayName: blockName,
+        description: "This is a generated block with the internal name of " + genID,
+        size: 1,
+        buildVisibility: BuildVisibility.shown
+    });
+    genBlocks.add(b);
+    print("generated " + genID);
+}
+
+// Set the textures, doesn't work in the loop
+// TODO: Set icons for generated blocks
+Events.on(ClientLoadEvent, () => {
+    genBlocks.each(b => {
+        b.region = Core.atlas.find("router");
+    });
+});
+
+print("Generated [stat]" + i + "[] blocks");
